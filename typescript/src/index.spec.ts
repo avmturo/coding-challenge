de70d1504ea853cf8cdf4bcdc210db0094d78cb0
avmturo <30347978+avmturo@users.noreply.github.com>
2022-09-05 16:28:24 +0100

    Submission: Implementation of the Typescript code problem.

--- typescript/src/index.spec.ts
+++ typescript/src/index.spec.ts
@@ -23,6 +23,29 @@ const givenDivision = [
   },
 ];
 
+const divisionWithDuplicateNameInRelegatedAndPromoted = [
+  {
+    name: "Rockets",
+    points: 64,
+  },
+  {
+    name: "Bruisers",
+    points: 77,
+  },
+  {
+    name: "Bruisers",
+    points: 51,
+  },
+  {
+    name: "Renegades",
+    points: 37,
+  },
+  {
+    name: "Porpoises",
+    points: 52,
+  },
+];
+
 test("returns one team to promote and one team to relegate", () => {
   const resultString = `Promote:
 Cardinals
@@ -43,4 +66,16 @@ Bruisers
 Renegades`;
 
   expect(getResults(givenDivision, 2)).toBe(resultString);
+});
+
+test("returns notification string if n is greater than half the division size", () => {
+  const resultString = 'Too many teams being relegated and promoted'
+
+  expect(getResults(givenDivision, 3)).toBe(resultString);
+});
+
+test("returns notification string if there is overlap in the relegated and promoted teams", () => {
+  const resultString = 'Overlap in relegated and promoted teams'
+
+  expect(getResults(divisionWithDuplicateNameInRelegatedAndPromoted, 2)).toBe(resultString);
 });
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,36 @@
-export const getResults = (division: unknown, n: unknown) => {
-  // Your code here
+type TeamStats = {
+    name: String;
+    points: number;
+}
+
+type Division = Array<TeamStats>
+
+export const getResults = (division: Division, n: number): String => {
+    // To avoid checks later on, if n is greater than half the number of teams there will be overlaps
+    if(n > division.length / 2) {
+        return 'Too many teams being relegated and promoted'
+    }
+    // O(n log n) complexity here, which given the max length of 16 defined in the use case, isn't a problem. This
+    // uses all Array.Prototype functions, and would be clear to any other front-end dev.
+    // You could achieve better by doing a single loop through division, and comparing with two Arrays of n length
+    // which would hold the n smallest and n largest values, comparing with each value and shift-inserting the value
+    // when the compare is less than or greater than (respectively) the n[i].
+    // Followed by popping the last value, to maintain size n.
+    // Given the restriction division max length = 16, this would require more code, our own shift insert logic,
+    // and would take away from the readability of the below, without offering enough in complexity improvement.
+    division.sort((a,b) => b.points - a.points);
+    const firstNTeamNames = division.slice(0, n).map(({name}) => name)
+    const lastNTeamNames = division.slice(-n).map(({name}) => name)
+
+    // check that there are no overlaps
+    for (const name of firstNTeamNames) {
+        if (lastNTeamNames.includes(name)) {
+            return 'Overlap in relegated and promoted teams'
+        }
+    }
+
+    // Though not in the useCase constraints, you would probably want to run the above check on the points also,
+    // or have some sort of resolution in the case when there are many teams with the same points.
+
+    return `Promote:\n${firstNTeamNames.join('\n')}\n\nRelegate:\n${lastNTeamNames.join('\n')}`
 };
